aebe05dbe3d92c4f63ab255673ee5fafd9ce36e0
Luka_Hat <luka.hat58@gmail.com>
2023-12-13 21:57:35 +0100

    add search bar

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div className="searchContainer">
+      <input
+        type="text"
+        placeholder="Enter song name..."
+        id="searchBar"
+        onChange={handleInput}
+        className="searchBar"
+      />
+      <button className="searchButton">Search</button>
+      <p>{input}</p>
+    </div>
+  );
 };
 
 export default SearchBar;
